cc4970153c723067607aaf381b53b3dacecc40ad
M7amedNahhas <m7amednahhas@gmail.com>
2017-11-18 15:20:44 +0300

    v0.1 adding the picker with types name

--- ProjectMips/ProjectMips/ViewController.swift
+++ ProjectMips/ProjectMips/ViewController.swift
@@ -8,11 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+   
+    
 
+   
+    @IBAction func addNewTypeBt(_ sender: UIButton) {
+        
+    }
+    
+    //picker methods
+    @IBOutlet weak var TypesPicker: UIPickerView!
+    let pickerData = [["R-Type","I-Type","J-Type"]]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+    }
+    
+    //implement picker delegate
+    func pickerView(_
+        pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int
+        ) -> String? {
+        return pickerData[component][row]
+    }
+   
+    
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        TypesPicker.delegate = self
+        TypesPicker.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
